015d6aba8ebf5fce52a0141ce7a4ea43b26b4750
SaraBarrett <sara.c.mon@gmail.com>
2025-03-12 16:10:00 +0000

    first

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Olá Mundo
+    </h1>
+
+    <?php
+        $myFirstVar = 'olá mundo';
+        $myFirstNumber = 16;
+
+        $myFirstVar= 'Sara';
+        $myFirstVar = 2025;
+
+        $mySecondNumber = 55;
+
+        //echo $myFirstVar;
+        echo $myFirstNumber .'e'.$mySecondNumber;
+    ?>
+    
+</body>
+</html>
